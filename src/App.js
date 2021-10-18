c207d6a73a18e827f8f9c07a2fe304accd6b9358
andregbracco <andygbracco@gmail.com>
2021-10-18 01:30:11 -0300

    tierlist

--- src/App.js
+++ src/App.js
@@ -3,22 +3,98 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="tabla">
+      <div className="row cabecera">
+        <div className="col">
+          Calificame
+        </div>
+        <div className="pelis" >
+          Películas
+        </div>
+        <div className="col" >
+          Cantidad
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          Excelente
+        </div>
+        <div className="pelis">
+          <div className="cardpeli">lal</div>
+            
+        </div>
+        <div className="col">
+          0
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          Bueno
+        </div>
+        <div className="pelis">
+
+        </div>
+        <div className="col">
+          0
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          Regulero
+        </div>
+        <div className="pelis">
+
+        </div>
+        <div className="col">
+          0
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          Malo
+        </div>
+        <div className="pelis">
+
+        </div>
+        <div className="col">
+          0
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          Caca
+        </div>
+        <div className="pelis">
+
+        </div>
+        <div className="col">
+          0
+        </div>
+      </div>
+      <h2>Películas:</h2>
+      <div className="conteiner-pelis">
+        <div className="cardpeli">bomboncito</div>
+        <div className="cardpeli">lindo</div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+        <div className="cardpeli"></div>
+
+      </div>
+      <div className="agregar">
+        <label>Nueva Película:</label>
+        <input type="text" className="pelinueva" placeholder="Peli..."></input>
+        <button >Agregar</button>
+      </div>
+  </div>
+
   );
 }
 
